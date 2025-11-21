15d19beadcde08cb8d084a2532467bf101f993d1
Caio Fitipaldi <caiofitipaldi@gmail.com>
2025-11-20 21:22:00 -0300

    Controle de Atitude com arquivos de cabeçalho

---
+++ IaraSat/Controle de Atitude/controleDeAtitudeIaraSat_Testes/pinos_gigaTeste.h
@@ -0,0 +1,19 @@
+// pinos_gigaTeste.h
+
+// ===========================================
+// ------- PINS DO HARDWARE -------
+// ===========================================
+
+#define LED 13      // LED de status
+#define IN1 5            // Pino de controle 1 do driver de motor (PWM)
+#define IN2 6            // Pino de controle 2 do driver de motor (PWM)
+#define LDRVERMELHO A0  // Sensor LDR 0 (Analog 0), Direita
+#define LDRAZUL A1      // Sensor LDR 1 (Analog 1), Esquerda
+#define SWANT1    A2 // chave de fim de curso da Antena (Abrir)
+#define SWANT2    A3 //chave de fim de cursso Antena (fechar)
+#define ANTIN1 3 // IN1 Pino de controle 1 do driver de motor da Antena
+#define ANTIN2 5 // IN2 Pino de controle 1 do driver de motor
+#define PAINEL_IN1 9 // controle do Painel (+5V)
+#define PAINEL_IN2 6 //
+#define PAINEL_IN3 7 // controle do Painel (+7.4V)
+#define PAINEL_IN4 8 // 
